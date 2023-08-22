e99d58d49412ab4ba3733a588dfc896435d358cf
Pietro-Leone <leonepietro1999@gmail.com>
2023-08-22 16:53:53 +0200

    end header

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script>
+import Header from "./components/TheHeader.vue"
+import Footer from "./components/TheFooter.vue"
+export default {
+  components: {
+    Header,
+    Footer,
+  }
+}
+</script>
+
+<template>
+  <Header />
+
+  <main>
+    
+  </main>
+
+  <Footer />
+</template>
+
+<style></style>
---
+++ src/components/TheFooter.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+
+}
+</script>
+
+<template>
+  
+  <footer>
+
+    
+
+  </footer>
+
+</template>
+
+<style lang="scss" scoped>
+
+</style>
